25ca3172352ac41a0e4372595ab01a600217c75d
Arseniy Tashoyan <tashoyan@gmail.com>
2018-12-26 17:35:28 +0300

    Add some dependencies

--- scala-lib/src/main/scala/com/github/tashoyan/demo/Lib.scala
+++ scala-lib/src/main/scala/com/github/tashoyan/demo/Lib.scala
@@ -1,8 +1,12 @@
 package com.github.tashoyan.demo
 
+import org.apache.commons.lang3.Validate
+
 object Lib {
 
-  def reverseString(str: String): String =
+  def reverseString(str: String): String = {
+    Validate.notNull(str, "Argument must not be null")
     str.reverse
+  }
 
 }
