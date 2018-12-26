3f631e3aac59e3f6cc19e6bb53537f6767234084
Arseniy Tashoyan <tashoyan@gmail.com>
2018-12-26 17:35:06 +0300

    Fix: java_properties

--- package-resources/src/main/resources/rpm/default-conf/build-infra-demo-env.sh
+++ package-resources/src/main/resources/rpm/default-conf/build-infra-demo-env.sh
@@ -7,3 +7,6 @@ set -o pipefail
 
 # Path to Apache Groovy distribution.
 export GROOVY_HOME="$install_dir/@{distribution.groovy.install.dir}"
+
+# Java system properties used when starting the application.
+java_properties="-server -XX:+HeapDumpOnOutOfMemoryError"
