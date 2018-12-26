54ac9995a2414f31112891c2130ef0ac50e277d6
Arseniy Tashoyan <tashoyan@gmail.com>
2018-12-26 21:54:59 +0300

    Add logging in the lib

--- scala-lib/src/main/scala/com/github/tashoyan/demo/Lib.scala
+++ scala-lib/src/main/scala/com/github/tashoyan/demo/Lib.scala
@@ -1,11 +1,29 @@
 package com.github.tashoyan.demo
 
 import org.apache.commons.lang3.Validate
+import org.slf4j.{Logger, LoggerFactory}
 
+/**
+  * Dummy library.
+  */
 object Lib {
 
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  /**
+    * Returns reverse string.
+    *
+    * @param str String to reverse.
+    * @return Reverse string.
+    * @throws NullPointerException Argument is null.
+    */
   def reverseString(str: String): String = {
     Validate.notNull(str, "Argument must not be null")
+
+    if (logger.isDebugEnabled) {
+      logger.debug("reverse for %s", str)
+    }
+
     str.reverse
   }
 
